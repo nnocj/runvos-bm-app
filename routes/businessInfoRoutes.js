5aec32bae484d0594272b801b31353f7b77cba44
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-08-07 05:28:07 +0000

    gh

--- routes/businessInfoRoutes.js
+++ routes/businessInfoRoutes.js
@@ -1,22 +1,22 @@
-const router = require('express').Router();
+const router =  new require('express').Router();
 const businessController = require('../controller/businessController');
 const businessValidate = require('../utilities/validateBusiness.js');
 const  errorHandler = require('../middleware/handleErrors');
 
 // Serve static files from the public folder
 // This endpoint retrieves all businesses from the MongoDB database
-router.get('/api/businesses',   errorHandler.generalHandleErrors(businessController.getAllBusinesses));
+router.get('/',   errorHandler.generalHandleErrors(businessController.getAllBusinesses));
 
 // This endpoint retrieves all businesses from the MongoDB database
-router.get('/api/businesses/:id', errorHandler.generalHandleErrors(businessController.getBusinessById));
+router.get('/:id', errorHandler.generalHandleErrors(businessController.getBusinessById));
 
 // This endpoint creates a new business in the MongoDB database
-router.post('/api/businesses', businessValidate.businessValidationRules(), businessValidate.validateResults, errorHandler.generalHandleErrors(businessController.postBusiness));
+router.post('/', businessValidate.businessValidationRules(), businessValidate.validateResults, errorHandler.generalHandleErrors(businessController.postBusiness));
 
 // This endpoint updates an existing business in the MongoDB database based on the id
-router.put('/api/businesses/:id', businessValidate.businessValidationRules(), businessValidate.validateResults, errorHandler.generalHandleErrors(businessController.putBusiness));
+router.put('/:id', businessValidate.businessValidationRules(), businessValidate.validateResults, errorHandler.generalHandleErrors(businessController.putBusiness));
 
 // This endpoint deletes a business from the MongoDB database based on the id
-router.delete('/api/businesses/:id', errorHandler.generalHandleErrors(businessController.deleteBusiness));
+router.delete('/:id', errorHandler.generalHandleErrors(businessController.deleteBusiness));
 
 module.exports = router;
