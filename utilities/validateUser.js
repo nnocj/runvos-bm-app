f37797c7653c90309dd92c41a13e7cbf17301a86
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-08-07 07:09:23 +0000

    adjusting the  user validation error code

--- utilities/validateUser.js
+++ utilities/validateUser.js
@@ -28,7 +28,7 @@ const validateResults = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({
+    return res.status(400).json({
       status: 'fail',
       errors: errors.array()
     });
