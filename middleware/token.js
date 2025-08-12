fdc986f0c8e67d16db4c253369a352f8a85ad704
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-08-12 08:37:51 +0000

    correct  jwt

--- middleware/token.js
+++ middleware/token.js
@@ -1,12 +1,16 @@
 const jwt = require('jsonwebtoken');
 
+const refreshTokens = new Set();
+
 function generateTokens(user) {
   const payload = { id: user._id, email: user.email, name: user.firstName + ' ' + user.lastName };
 
   const accessToken = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
   const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '7d' });
 
+  refreshTokens.add(refreshToken);  // Add refresh token to the set for validation
+
   return { accessToken, refreshToken };
 }
 
-module.exports = { generateTokens };
+module.exports = { generateTokens, refreshTokens };
