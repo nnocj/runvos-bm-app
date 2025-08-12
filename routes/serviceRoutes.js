7547ae0126d9dfb6139485b2b85c451be629256f
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2025-08-12 15:13:19 +0000

    adding auth to service creation routes

--- routes/serviceRoutes.js
+++ routes/serviceRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const serviceController = require('../controller/serviceController');
 const errorHandler = require('../middleware/handleErrors');
+const jwtVerify = require('../middleware/jwt');
 
-router.post('/', errorHandler.generalHandleErrors(serviceController.postService));
+router.post('/', jwtVerify.verifyJWT(), errorHandler.generalHandleErrors(serviceController.postService));
 router.get('/business/:businessId', errorHandler.generalHandleErrors(serviceController.getServicesByBusiness));
 router.put('/:id', errorHandler.generalHandleErrors(serviceController.putService));
 router.delete('/:id', errorHandler.generalHandleErrors(serviceController.deleteService));
